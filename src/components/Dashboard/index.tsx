3f206b53a8bd8a46a01b77c8cd9c14053388421e
Betty985 <1830265753@qq.com>
2022-11-02 14:59:47 +0800

    fix(custom): 手机端菜单唤出修复

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -6,8 +6,9 @@ import { MyBreadcrumb } from './MyBreadcrumb';
 import { MyToolBar } from './MyToolbar';
 import { MyTabs } from './MyTabs';
 import { useTheme } from '@/hooks';
-import Icon, { SettingOutlined } from '@ant-design/icons';
+import Icon, { SettingOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { SwitchChangeEventHandler } from 'antd/lib/switch';
 const { useBreakpoint } = Grid;
 
 const { Header, Content, Sider } = Layout;
@@ -28,24 +29,110 @@ const menu = (
         ]}
     />
 );
-const Dashboard: FC = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const [theme, setTheme] = useState<'dark' | 'light'>('dark');
-    const [mode, setMode] = useState<'vertical' | 'horizontal' | 'inline'>('inline');
-    const screens = useBreakpoint();
+const Toolbar: FC<{ theme: 'dark' | 'light'; changeTheme: SwitchChangeEventHandler }> = (props) => {
+    const { theme, changeTheme } = props;
     const [open, setOpen] = useState(false);
-    const { setLight } = useTheme();
-    const changeTheme = (value: boolean) => {
-        const sun = value ? 'dark' : 'light';
-        setTheme(sun);
-        setLight(sun);
-    };
+    const screens = useBreakpoint();
     const showDrawer = () => {
         setOpen(true);
     };
 
     const onClose = () => {
         setOpen(false);
+    };
+    return (
+        <Col flex="none">
+            <Row justify="end" gutter={{ xs: 8, sm: 16, md: 24 }}>
+                {screens.lg && <MyToolBar />}
+                <Col>
+                    <Switch
+                        checked={theme === 'dark'}
+                        onChange={changeTheme}
+                        checkedChildren="dark"
+                        unCheckedChildren="light"
+                    />
+                </Col>
+                <Col>
+                    <Dropdown overlay={menu} placement="bottom" arrow={{ pointAtCenter: true }}>
+                        <Avatar src="https://joeschmoe.io/api/v1/random" />
+                    </Dropdown>
+                </Col>
+                <Col>
+                    {!screens.lg && <SettingOutlined className="icon" onClick={showDrawer} />}
+                    <Drawer
+                        title="工具箱"
+                        placement="right"
+                        onClose={onClose}
+                        open={open}
+                        contentWrapperStyle={{ width: '50vw' }}
+                    >
+                        <MyToolBar box={true} />
+                    </Drawer>
+                </Col>
+            </Row>
+        </Col>
+    );
+};
+/** logo */
+const AdminLogo: FC = () => (
+    <>
+        <Icon component={Logo} className="fixed left-0.5 top-0.5" />
+        <span className="ml-8 bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l">
+            REACT ADMIN
+        </span>
+    </>
+);
+/** 菜单唤出 */
+const Trigger: FC<{ collapsed: boolean; setCollapsed: Function }> = (props) => {
+    const { collapsed, setCollapsed } = props;
+    const screens = useBreakpoint();
+    const [open, setOpen] = useState(false);
+
+    const onClose = () => {
+        setOpen(false);
+    };
+    return (
+        <>
+            <Drawer
+                title={<AdminLogo />}
+                placement="left"
+                onClose={onClose}
+                open={open}
+                contentWrapperStyle={{ width: '50vw', minWidth: '137px' }}
+                closable={false}
+            >
+                <MyMenu theme="light" />
+            </Drawer>
+            {collapsed ? (
+                <MenuUnfoldOutlined
+                    className="icon ml-7"
+                    onClick={() => {
+                        if (screens.sm) {
+                            setCollapsed((collapsed: boolean) => !collapsed);
+                        } else {
+                            setOpen(true);
+                        }
+                    }}
+                />
+            ) : (
+                <MenuFoldOutlined
+                    className="icon ml-4"
+                    onClick={() => setCollapsed((collapsed: boolean) => !collapsed)}
+                />
+            )}
+        </>
+    );
+};
+const Dashboard: FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const [theme, setTheme] = useState<'dark' | 'light'>('dark');
+    const [mode, setMode] = useState<'vertical' | 'horizontal' | 'inline'>('inline');
+    const screens = useBreakpoint();
+    const { setLight } = useTheme();
+    const changeTheme = (value: boolean) => {
+        const sun = value ? 'dark' : 'light';
+        setTheme(sun);
+        setLight(sun);
     };
     useEffect(() => {
         if (!screens.xl) {
@@ -55,20 +142,29 @@ const Dashboard: FC = () => {
     return (
         <Layout style={{ minHeight: '100vh' }} className="layout">
             <Header className={`${theme}`}>
-                <Tooltip title="点击切换菜单位置" color={'var(--ant-primary-color)'} defaultOpen>
-                    <Icon
-                        component={Logo}
-                        className="fixed left-2"
-                        onClick={() =>
-                            setMode((i) => {
-                                if (i === 'inline') {
-                                    return 'horizontal';
-                                }
-                                return 'inline';
-                            })
-                        }
-                    />
-                </Tooltip>
+                {screens.sm ? (
+                    <Tooltip
+                        title="点击切换菜单位置"
+                        color={'var(--ant-primary-color)'}
+                        defaultOpen
+                    >
+                        <Icon
+                            component={Logo}
+                            className="fixed left-2"
+                            onClick={() =>
+                                setMode((i) => {
+                                    if (i === 'inline') {
+                                        return 'horizontal';
+                                    }
+                                    return 'inline';
+                                })
+                            }
+                        />
+                    </Tooltip>
+                ) : (
+                    <Icon component={Logo} className="fixed left-2" />
+                )}
+
                 <Row
                     align="middle"
                     justify="space-between"
@@ -76,63 +172,32 @@ const Dashboard: FC = () => {
                     className="toolbar"
                     wrap={false}
                 >
-                    <Col>{!collapsed && <span className={`logo ${theme}`}>REACT ADMIN</span>}</Col>
+                    <Col>
+                        {!collapsed && screens.sm && (
+                            <span className={`logo ${theme}`}>REACT ADMIN</span>
+                        )}
+                        <Trigger collapsed={collapsed} setCollapsed={setCollapsed} />
+                    </Col>
+
+                    {/* 水平导航栏 */}
                     <Col>{mode === 'horizontal' && <MyMenu theme={theme} mode={mode} />}</Col>
+
                     {screens.xl && mode !== 'horizontal' ? (
                         <Col>
                             <MyBreadcrumb />
                         </Col>
                     ) : null}
-                    <Col flex="none">
-                        <Row justify="end" gutter={{ xs: 8, sm: 16, md: 24 }}>
-                            {screens.lg && <MyToolBar />}
-                            <Col>
-                                <Switch
-                                    checked={theme === 'dark'}
-                                    onChange={changeTheme}
-                                    checkedChildren="dark"
-                                    unCheckedChildren="light"
-                                />
-                            </Col>
-                            <Col>
-                                <Dropdown
-                                    overlay={menu}
-                                    placement="bottom"
-                                    arrow={{ pointAtCenter: true }}
-                                >
-                                    <Avatar src="https://joeschmoe.io/api/v1/random" />
-                                </Dropdown>
-                            </Col>
-                            <Col>
-                                {!screens.lg && (
-                                    <SettingOutlined className="icon" onClick={showDrawer} />
-                                )}
-                                <Drawer
-                                    title="工具箱"
-                                    placement="right"
-                                    onClose={onClose}
-                                    open={open}
-                                >
-                                    <MyToolBar box={true} />
-                                </Drawer>
-                            </Col>
-                        </Row>
-                    </Col>
+                    <Toolbar theme={theme} changeTheme={changeTheme} />
                 </Row>
             </Header>
 
             <Layout>
-                {mode === 'inline' && (
-                    <Sider
-                        collapsible
-                        collapsed={collapsed}
-                        onCollapse={(value) => setCollapsed(value)}
-                        theme={theme}
-                    >
+                {mode === 'inline' && screens.sm && (
+                    <Sider trigger={null} collapsible collapsed={collapsed} theme={theme}>
                         <MyMenu theme={theme} mode={mode} />
                     </Sider>
                 )}
-                <Content style={{ margin: '0 16px', overflowY: 'scroll' }}>
+                <Content style={{ margin: '0 16px' }}>
                     <MyTabs />
                 </Content>
             </Layout>
